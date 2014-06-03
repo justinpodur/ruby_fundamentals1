e1b47dd72e0918e59ea8c8335dbb02aa32f5b080
git-justinpodur <git-justin@podur.org>
2014-06-03 13:28:32 -0400

    completed exercise3.rb

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,13 @@ puts "What is your name?"
 name=gets.chomp
 
 puts "Hi #{name}!"
+
+puts "And how old might you be, #{name}?"
+
+age=gets.chomp
+
+puts "And what year is it?"
+
+year=gets.chomp
+
+puts "You are #{age} years old, and that means that you were born in the year of our lord #{year.to_i-age.to_i}."
